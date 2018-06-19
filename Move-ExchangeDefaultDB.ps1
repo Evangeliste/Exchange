23cdf19328f5a4fe51535b0b72d011f545976b24
Evangelistr <alexis@inventiq.fr>
2018-06-19 14:48:31 +0200

    UPDate

--- Move-ExchangeDefaultDB.ps1
+++ Move-ExchangeDefaultDB.ps1
@@ -1,3 +1,20 @@
+#########################################################################################################################
+#												                                                                        #
+#		Script for : Move Exchange Default DB to another volume and enable 'CircularLoggingEnabled'                   	#
+#																								                        #
+#		Script author : Alexis / Evangeliste										        	                        #
+#		mail 	: alexis.charriere@ai3.fr														                        #
+#		Create 	: 19 Juin 2018  																                        #
+#		Version	: 1.0																			                        #
+#		UPDate 	:																				                        #
+#																								                        #
+#       Usage : Move-ExchangeDefaultDB.ps1 -DatabasePath "D:\Microsoft Exchange\"                                       #
+#																								                        #
+#########################################################################################################################
+
+#Function/Module
+#########################################################################################################################
+
 [CmdletBinding(SupportsShouldProcess = $true)] 
 param ( 
     [string]$DatabasePath
