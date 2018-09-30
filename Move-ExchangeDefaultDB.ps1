2d0bf1bbf44b1477e11fe58c6242cc879da6799b
Evangelistr <alexis@inventiq.fr>
2018-09-30 22:22:24 +0200

    Add Debut mode
    UPDate 	: 1.1 : 30 Septembre 2018, prise en charge d'un mode debug par défaut, prise en charge du serveur d'execution

--- Move-ExchangeDefaultDB.ps1
+++ Move-ExchangeDefaultDB.ps1
@@ -5,8 +5,8 @@
 #		Script author : Alexis / Evangeliste
 #		mail 	: alexi@inventiq.fr
 #		Create 	: 19 Juin 2018
-#		Version	: 1.0
-#		UPDate 	:
+#		Version	: 1.1
+#		UPDate 	: 1.1 : 30 Septembre 2018, prise en charge d'un mode debug par défaut, prise en charge du serveur d'execution
 #
 #       Usage : Move-ExchangeDefaultDB.ps1 -DatabasePath "D:\Microsoft Exchange\"
 #
@@ -17,17 +17,52 @@
 
 [CmdletBinding(SupportsShouldProcess = $true)] 
 param ( 
-    [string]$DatabasePath
+    [string]$DatabasePath,
+    $DebugMode = $true
 )
 
+#Variables
+#########################################################################################################################
 $DataMove = Get-MailboxDatabase -Server $env:ComputerName
 $DBToMove = $DataMove.Name
 $DBFolder = ""+$DBToMove+"_DB"
 $LGFolder = ""+$DBToMove+"_LOG"
 
-Move-DatabasePath $DBToMove -EdbFilePath "$DatabasePath\$DBFolder\$DBToMove.edb" -LogFolderPath "$DatabasePath\$LGFolder\" -Confirm:$false -Force
+#Module
+#########################################################################################################################
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn;
 
-Set-MailboxDatabase $DataMove -CircularLoggingEnabled $true
+#Active Mode
+#########################################################################################################################
+IF ($DebugMode -eq $true)
+    {
+    Write-Host "#####################################################################################################################" -ForegroundColor Yellow
+    Write-Host "#                                                                                                                   #" -ForegroundColor Yellow
+    Write-Host "#                                       /!\ Running in Evaluate mode /!\                                            #" -ForegroundColor Yellow
+    Write-Host "#                                                                                                                   #" -ForegroundColor Yellow
+    Write-Host "#####################################################################################################################" -ForegroundColor Yellow
+    $errorActionPreference = "Continue"
+    }
+ELSE 
+    {
+    Write-Host "#####################################################################################################################" -ForegroundColor Green
+    Write-Host "#                                                                                                                   #" -ForegroundColor Green
+    Write-Host "#                                       /!\ Running in Active mode /!\                                              #" -ForegroundColor Red
+    Write-Host "#                                                                                                                   #" -ForegroundColor Green
+    Write-Host "#####################################################################################################################" -ForegroundColor Green
+    $errorActionPreference = "SilentlyContinue"
+    }
 
-Dismount-Database $DataMove -Confirm:$False
-Mount-Database $DataMove -Confirm:$False
+IF ($DebugMode -eq $true)
+    {
+    Move-DatabasePath $DBToMove -EdbFilePath "$DatabasePath\$DBFolder\$DBToMove.edb" -LogFolderPath "$DatabasePath\$LGFolder\" -Confirm:$false -Force -whatif
+    Set-MailboxDatabase $DataMove -CircularLoggingEnabled $true -whatif
+    }
+else
+    {
+    Move-DatabasePath $DBToMove -EdbFilePath "$DatabasePath\$DBFolder\$DBToMove.edb" -LogFolderPath "$DatabasePath\$LGFolder\" -Confirm:$false -Force
+    Set-MailboxDatabase $DataMove -CircularLoggingEnabled $true
+
+    Dismount-Database $DataMove -Confirm:$False
+    Mount-Database $DataMove -Confirm:$False
+    }
